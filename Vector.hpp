f9aa1757ffae6deff232539dfefd17d64fa4387c
razgrinblat <raznadavg@gmail.com>
2024-07-20 19:26:02 +0300

    add operator<< function and doxygen

--- Vector.hpp
+++ Vector.hpp
@@ -1,8 +1,7 @@
 #ifndef VECTOR_HPP
+
 #define VECTOR_HPP
 
-
-#include <cstdlib>
 #include <iostream>
 
 
@@ -10,150 +9,235 @@ template<typename T>
 class Vector
 {
 public:
+	/**
+	 * @brief Default constructor.
+	 *
+	 * Constructs an empty Vector with an initial capacity of 2.
+	 */
 	Vector()
 	{
 		Realloc(2);
 	}
-
+	/**
+	 * @brief Destructor.
+	 *
+	 * Frees the dynamically allocated memory.
+	 */
 	~Vector()
 	{
-		free(DynamicVector);
+
+		delete(_dynamicVector);
 	}
 
-
+	/*@brief Adds an element to the end of the vector.
+	*
+     **/
 	void pushBack(const T& value)
+		
 	{
-		if (size >= Capacity)
+		if (_size >= _capacity)
 		{
-			Realloc(Capacity + Capacity / 2);
+			Realloc(_capacity + _capacity / 2);
 
 		}
-		DynamicVector[size] = value;
-		size++;
+		_dynamicVector[_size] = value;
+		_size++;
 	}
 
+	/*display the Vector*/
 	void display() const
 	{
-		for (size_t i = 0; i < size; ++i) {
-			std::cout << DynamicVector[i] << " ";
+		for (size_t i = 0; i < _size; ++i) {
+			std::cout << _dynamicVector[i] << " ";
 		}
 		std::cout << std::endl;
 	}
 
-
+	/*@return returns the element in index i
+	@throw  std::out_of_range if the index is out of range*/
 	T& at(int i) const
 	{
-		if (i < 0 || i >= size)
+		if (i < 0 || i >= _size)
 		{
 			throw std::out_of_range("Index is out range");
 		}
-		return DynamicVector[i];
+		return _dynamicVector[i];
 	}
 
+	/*@return returns the element in index i*/
 	T& operator[](int i)
 	{
 
-		return DynamicVector[i];
+		return _dynamicVector[i];
 	}
 
-	T& front()
+	
+	/**
+	 * @brief Overloads the equality operator to compare two vectors.
+	 *
+	 * @param other- The vector to compare with this vector.
+	 * @return True if the vectors are equal, false otherwise.
+	 */
+	bool operator==(const Vector& other) const
 	{
-		if (size > 0)
+		if (_size != other._size)
 		{
-			return DynamicVector[0];
-		}
-	}
-
-	T& back()
-	{
-		if (size > 0)
-		{
-			return DynamicVector[size - 1];
-		}
-
-	}
-
-	bool empty()
-	{
-		if (size == 0)
-		{
-			return true;
-		}
-		else {
 			return false;
 		}
+		for (size_t i = 0; i < _size; ++i) {
+			if (_dynamicVector[i] != other._dynamicVector[i]) {
+				return false;
+			}
+		}
+		return true;
+	
+	}
+		/**
+	* @brief Overload of the << operator to print the contents of a Vector.
+	* @param os The output stream to which the contents are printed.
+	* @param vec The Vector whose contents are to be printed.
+	* @return std::ostream& A reference to the output stream.
+	*/
+	 friend std::ostream& operator<<(std::ostream& os, const Vector& vec) 
+	{	
+		os << "[";
+		for (size_t i = 0; i < vec._size; ++i) 
+		{
+			os << vec._dynamicVector[i];
+			if (i != vec._size - 1) {
+				os << ", ";
+			}
+		}
+		os << "]";
+		return os;
+	 }
+
+	/*@return returns the first element in the Vector */
+	T& front()
+	{
+		if (_size > 0)
+		{
+			return _dynamicVector[0];
+		}
+	}
+
+	/*@return returns the last element in the Vector */
+	T& back()
+	{
+		if (_size > 0)
+		{
+			return _dynamicVector[_size - 1];
+		}
+
+	}
+
+	/*@brief check if the Vector is empty
+	@return retrun true if the Vecor is empty, otherwise return false*/
+	bool empty()
+	{
+		return _size == 0;
 	}
 
 	void clear()
 	{
 
-		free(DynamicVector);
-		DynamicVector = nullptr;
-		size = 0;
-		Capacity = 0;
+		delete(_dynamicVector);
+		_dynamicVector = nullptr;
+		_size = 0;
+		_capacity = 0;
 	}
+	/*
+	@brief Inserts an element at the specified index in the vector.
+	@param index The position where the element should be inserted. Must be between 0 and the current size (inclusive).
+ *	@param value A constant reference to the element of type T to be inserted at the specified index.
+ *	@throw std::out_of_range if the index is out of range.*/
 
-	void insert(int index, const T& value)
+	void insert(int index, const T& value) 
 	{
-		if (index < 0 || index > size)
+		if (index < 0 || index > _size)
 		{
 			throw std::out_of_range("Index out of range");
 		}
-		int curr_space = Capacity - size;
+		int curr_space = _capacity - _size;
 		if (curr_space == 0)
 		{
-			Realloc(Capacity + Capacity / 2);
+			Realloc(_capacity + _capacity / 2);
 		}
-		for (int i = size; i > index; i--)
+		for (int i = _size; i > index; i--)
 		{
-			DynamicVector[i] = DynamicVector[i - 1];
+			_dynamicVector[i] = _dynamicVector[i - 1];
 		}
-		DynamicVector[index] = value;
-		size++;
+		_dynamicVector[index] = value;
+		_size++;
 
 	}
 
+	/**
+	 * @brief Gets the current size of the vector.
+	 *
+	 * @return The current number of elements in the vector.
+	 */
 	size_t Size() const
 	{
-		return size;
-	}
-	size_t capacity() const
-	{
-		return Capacity;
+		return _size;
 	}
 
+	/**
+	 * @return The current capacity in the vector.
+	 */
+	size_t capacity() const
+	{
+		return _capacity;
+	}
+
+	/*@brief reverse the Vector by place*/
 	void reverse() const
 	{
 		T value;
-		for (int i = 0; i < size / 2; i--)
+		for (int i = 0; i < _size / 2; i--)
 		{
-			value = DynamicVector[i];
-			DynamicVector[i] = DynamicVector[size - i - 1];
-			DynamicVector[size - i - 1] = value;
+			value = _dynamicVector[i];
+			_dynamicVector[i] = _dynamicVector[_size - i - 1];
+			_dynamicVector[_size - i - 1] = value;
 		}
 
 	}
 
+	/**
+	 * @brief Removes the last element of the vector.
+	 *
+	 * Reduces the size of the vector by one. If the vector is empty, it throws an exception.
+	 *
+	 * @throw std::out_of_range if the vector is empty.
+	 */
 	void pop_back()
 	{
-		DynamicVector = (T*)realloc(DynamicVector, sizeof(T) * (Capacity - 1));
-		size--;
-		Capacity--;
-
+		if (_size == 0)
+		{
+			throw std::out_of_range("Vector is empty");
+		}
+		_size--;
 	}
 
 
+
 private:
-	T* DynamicVector = nullptr;
-	size_t Capacity = 0;
-	size_t size = 0;
+	T* _dynamicVector = nullptr;
+	size_t _capacity = 0;
+	size_t _size = 0;
 
 	void Realloc(size_t newCapacity)
 	{
+	
+		T* newVector = new T[newCapacity];
+		for (size_t i = 0; i < _size; i++)
+		{
+			newVector[i] = std::move(_dynamicVector[i]);
+		}
+		delete[] _dynamicVector;
 
-		DynamicVector = (T*)realloc(DynamicVector, newCapacity * sizeof(T));
-		Capacity = newCapacity;
-
+		_dynamicVector = newVector;
+		_capacity = newCapacity;
 	}
 
 
--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include "Vector.hpp"
 
 int main() {
-    Vector<int> myVector; 
+    Vector<int> myVector;
+    Vector<int> myVector2;
+
+    myVector2.pushBack(20);
+    myVector2.pushBack(30);
+    myVector2.pushBack(10);
+
+ 
 
     
-    myVector.pushBack(10);
     myVector.pushBack(20);
-    myVector.insert(1, 30); 
-    myVector.display(); 
-    myVector.reverse();
+    myVector.pushBack(10);
+    myVector.insert(1, 30);
+    myVector.pop_back();
     myVector.display();
+    
+    std::cout << "Size: " << myVector.Size() << std::endl;
+    bool x = myVector == myVector2;
+    std::cout << myVector2 << std::endl;
 
     return 0;
 }
